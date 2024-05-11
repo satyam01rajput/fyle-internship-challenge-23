6967ff954d00ce6b8bf2dde065156b46fd54b31f
satyam01rajput <satyam01rajput@gmail.com>
2024-05-11 14:02:01 +0530

    Pre Final

---
+++ src/model/GitHubUser.ts
@@ -0,0 +1,9 @@
+export type GitHubUser =  {
+    id:number
+    name: string
+    avatar_url: string
+    bio: string
+    location: string
+    twitter_username: string
+    html_url: string
+}
---
+++ src/model/Repo.ts
@@ -0,0 +1,8 @@
+export type Repo = {
+    id:number
+    name:string
+    description: string
+    html_url: string
+    language: string
+    topic: string[]
+}
